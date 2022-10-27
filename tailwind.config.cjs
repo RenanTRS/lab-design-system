5de7f446ba52859b10c7c2cdf8929b1f8483f9a5
Renan Souza <renantrsouza@gmail.com>
2022-10-27 12:13:43 -0400

    style: create tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,14 +13,17 @@ module.exports = {
       '2xl': 32
     },
     colors: {
-      'gray': {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      gray: {
         100: '#e1e1e6',
         200: '#c4c4cc',
         400: '#7c7c8a',
         800: '#202024',
         900: '#121214'
       },
-      'cyan': {
+      cyan: {
         300: '#9be1fb',
         500: '#81d8f7'
       }
